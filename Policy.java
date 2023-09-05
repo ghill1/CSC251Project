c773cfc3cdf5e4b7f4e2f937699dd9b98170b889
ghill1 <ghill012@email.cpcc.edu>
2023-09-04 23:22:47 -0400

    Updated Methods
    Added necessary methods and updated existing methods.

--- Policy.java
+++ Policy.java
@@ -1,4 +1,188 @@
 public class Policy
 {
+   private int polNumber; //policy number
+   private int polAge;  //policyholder's age
+   private double polHeight; //policyholder's height
+   private double polWeight; //policyholder's weight
+   private double userBMI; //policyholder's BMI (body mass index)
+   private double policyPrice; //price of policy with all fees included
+   private String providerName; //insurance provider
+   private String polFirstName; //policyholder's first name
+   private String polLastName; //policyholder's last name
+   private String polSmokingStatus; //policyholder's smoking status
    
+   //no-arg constructor, sets all fields to default value
+   public Policy()
+   {  
+      polNumber = 0;
+      polAge = 0;
+      polHeight = 0;
+      polWeight = 0;
+      providerName = " ";
+      polFirstName = " ";
+      polLastName = " ";
+      polSmokingStatus = " ";
+   }   
+   /**
+      constructor that accepts all necessary arguments 
+      @param pNum policy number
+      @param pName insurance provider 
+      @param fName policyholder's first name
+      @param lName policyholder's last name
+      @param pAge policyholder's age
+      @param smokeStatus policyholder's smoking status
+      @param pHeight policyholder's height
+      @param pWeight policyholder's weight
+   */      
+   public Policy(int pNum, String pName, String fName,String lName,int pAge,String smokeStatus, double pHeight, double pWeight)
+   {
+      polNumber = pNum;
+      providerName = pName;
+      polFirstName = fName;
+      polLastName = lName;
+      polAge = pAge;
+      polSmokingStatus = smokeStatus;
+      polHeight = pHeight;
+      polWeight = pWeight; 
+   }       
+   /**
+      setPolicyNumber method will change the value of polNumber
+      @param pNumber policy number
+   */
+   public void setPolicyNumber(int pNumber)
+   {
+      polNumber = pNumber;
+   }
+   /**
+      setProviderName method will change the value of providerName
+      @param pName insurance provider
+   */
+   public void setProviderName(String pName)
+   {
+      providerName = pName;
+   }
+   /**
+      setFirstName method will change the value of polFirstName
+      @param fName policyholder's first name
+   */ 
+   public void setFirstName(String fName)
+   {
+      polFirstName = fName;
+   }
+   /**
+      setLastName method will change the value of polLastName
+      @param lName policyholder's last name
+   */
+   public void setLastName(String lName)
+   {
+      polLastName = lName;
+   }
+   /**
+      setAge method will change the value of polAge
+      @param pAge
+   */ 
+   public void setAge(int pAge)
+   {
+      polAge = pAge;
+   }
+   /**
+      setSmokingStatus method will change the policyholder's smoking status
+      @param smokeStatus
+   */
+   public void setSmokingStatus(String smokeStatus)
+   {
+      polSmokingStatus = smokeStatus;
+   }
+   /**
+      setHeight method will change the value of polHeight
+      @param pHeight policyholder's height
+   */
+   public void setHeight(double pHeight)
+   {
+      polHeight = pHeight;
+   }
+   /**
+      setWeight method will change the value of polWeight
+      @param pWeight policyholder's weight
+   */
+   public void setWeight(double pWeight)
+   {
+      polWeight = pWeight;
+   }
+                                   
+   public int getPolicyNumber()
+   {
+      return polNumber;         
+   } 
+     
+   public String getProviderName()
+   {
+      return providerName;
+   } 
+     
+   public String getFirstName()
+   {
+      return polFirstName;
+   } 
+    
+   public String getLastName()
+   {  
+      return polLastName;
+   } 
+     
+   public int getAge()
+   {
+      return polAge;
+   } 
+     
+   public String getSmokeStatus()
+   {
+      return polSmokingStatus;
+   } 
+     
+   public double getHeight()
+   {
+      return polHeight;
+   } 
+    
+   public double getWeight()
+   {
+      return polWeight;
+   } 
+   
+   public double getBMI()     
+   {
+      final int SEVENHUNDREDTHREE = 703;
+      userBMI = (polWeight * SEVENHUNDREDTHREE) / (polHeight * polHeight);
+      return userBMI;
+   }
+   
+   public double getPolicyPrice()
+   {
+      final int TWENTY = 20;
+      final int THIRTYFIVE = 35;
+      final int FIFTY = 50;
+      final int SEVENTYFIVE = 75;
+      final int HUNDRED = 100;
+      double feeBMI;
+      policyPrice = 600;
+      
+      
+      if(polAge > FIFTY)
+      {
+         policyPrice += SEVENTYFIVE;
+      }
+      
+      if(polSmokingStatus == "smoker" || polSmokingStatus == "Smoker")
+      {
+         policyPrice += HUNDRED;
+      }
+      
+      if(userBMI > THIRTYFIVE)
+      {
+         feeBMI = (userBMI - THIRTYFIVE) * TWENTY;
+         policyPrice += feeBMI;
+      }
+      return policyPrice;  
+   }
 }
--- Project_Giani_Hill.java
+++ Project_Giani_Hill.java
@@ -1,4 +1,71 @@
+import java.util.*;
+
+/**
+   this program will compute an insurance policy by creating an instance of the
+   Policy class and allowing the user to enter the required information and be 
+   presented with a unique insurance policy
+*/
 public class Project_Giani_Hill
 {
-
+   public static void main(String[] args)
+   {
+      //variables to hold user input and pass as arguments to methods
+      int polNumber;
+      int polAge;
+      double polHeight;
+      double polWeight;
+      double userBMI;
+      String providerName;
+      String polFirstName;
+      String polLastName;
+      String polSmokingStatus;
+      
+      //create scanner object for user input
+      Scanner key = new Scanner(System.in);
+      
+      //prompt the user to enter all necessary information
+      System.out.print("Please enter the Policy number: ");
+      polNumber = key.nextInt();
+      
+      System.out.print("Please enter the Provider's name: ");
+      key.nextLine();
+      providerName = key.nextLine();
+      
+      System.out.print("Please enter the Policyholder's First name: ");
+      polFirstName = key.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Last name: ");
+      polLastName = key.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Age: ");
+      polAge = key.nextInt();
+      
+      System.out.print("Please enter the Policyholder's Smoking status (smoker/non-smoker): ");
+      key.nextLine();
+      polSmokingStatus = key.nextLine();
+      
+      System.out.print("Please enter the Policyholder's Height (in inches): ");
+      polHeight = key.nextDouble();
+      
+      System.out.print("Please enter the Policyholder's Weight (in pounds): ");
+      polWeight = key.nextDouble();
+      
+      //create instance of Policy class Policy1 and pass user input as arguments
+      Policy Policy1 = new Policy(polNumber, providerName, polFirstName, polLastName, polAge, polSmokingStatus, polHeight, polWeight);
+      
+      //display all the user's entered information as well as their calculated BMI and policy price
+      System.out.print("\nPolicy Number: " + Policy1.getPolicyNumber() + "\n");
+      System.out.print("Provider Name: " + Policy1.getProviderName() + "\n");
+      System.out.print("Policyholder's First Name: " + Policy1.getFirstName() + "\n");
+      System.out.print("Policyholder's Last Name: " + Policy1.getLastName() + "\n");
+      System.out.print("Policyholder's Age: " + Policy1.getAge() + "\n");
+      System.out.print("Policyholder's Smoking Status: " + Policy1.getSmokeStatus() + "\n");
+      System.out.print("Policyholder's Height: " + Policy1.getHeight() + " inches\n");
+      System.out.print("Policyholder's Weight: " + Policy1.getWeight() + " pounds\n");
+      System.out.printf("Policyholder's BMI: %.2f\n", Policy1.getBMI());
+      System.out.printf("Policy Price: $%,.2f\n", Policy1.getPolicyPrice()); 
+      
+   }//end main
+   
+      
 }
